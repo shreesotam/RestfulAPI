056bfeba71f86381581341fe1c7b5b693447d326
shreesottam keshari <shreesottamkumar@gmail.com>
2024-10-06 20:31:27 +0300

    pushing the api

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestfulAPI.Models;
+
+namespace RestfulAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure the Price property
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18, 2)"); // You can adjust precision and scale as needed
+        }
+    }
+}
